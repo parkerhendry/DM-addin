40aa0e932199a21a9d051c3f1d2aecbc7ce45c71
Parker Hendry <parker@traxxisgps.com>
2025-08-27 11:21:37 -0400

    Preflight check

--- netlify/functions/get-device-list.js
+++ netlify/functions/get-device-list.js
@@ -2,6 +2,17 @@ exports.handler = async (event, context) => {
   const DM_API_BASE = 'https://api.oemserver.com/v1';
   const DM_API_TOKEN = 'hUpEcwaVfthLqxMOP8MirN.tFoswRLau5YFaBRTicD_vUt2TKc8_LefBgLK7J1a02w7.1';
 
+  if (event.httpMethod === 'OPTIONS') {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS'
+    }
+  };
+}
+
   try {
     const response = await fetch(`${DM_API_BASE}/TrackingDevice/GetDeviceList`, {
       method: 'GET',
